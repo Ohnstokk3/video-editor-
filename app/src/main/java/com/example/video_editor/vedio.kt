566edb4729a4e2e2da2184b719aae8a642d2a43f
mohamed hassan <hh71728@gmail.com>
2024-10-03 19:15:15 +0300

    0.03-added Listener

--- app/src/main/java/com/example/video_editor/vedio.kt
+++ app/src/main/java/com/example/video_editor/vedio.kt
@@ -1,60 +1,146 @@
 package com.example.video_editor
 
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.annotation.OptIn
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 
 @OptIn(UnstableApi::class)
 @Composable
 fun VideoPlayerExo() {
-
     val context = LocalContext.current
-
-    /** Create the player and set the media item with clipping configuration */
-    val player = ExoPlayer.Builder(context).build().apply {
-        setMediaItem(
-            MediaItem.Builder()
-                .setUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
-                .setClippingConfiguration(
-                    MediaItem.ClippingConfiguration.Builder()
-                        .setStartPositionMs(200000) // Start at 10 seconds
-                        .setEndPositionMs(600000) // End at 20 seconds
-                        .build()
-                )
-                .build()
-        )
-        prepare()
-        playWhenReady
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+                setMediaItem(MediaItem.fromUri("https://firebasestorage.googleapis.com/v0/b/jetcalories.appspot.com/o/FROM%20EARTH%20TO%20SPACE%20_%20Free%20HD%20VIDEO%20-%20NO%20COPYRIGHT.mp4?alt=media&token=68bce5a0-7ca7-4538-9fea-98d0dc2d3646"))
+                prepare()
+                play()
+            }
     }
 
-    val playerView = PlayerView(context)
-    playerView.player = player
+    var isPlaying by remember {
+        mutableStateOf(false)
+    }
 
-    AndroidView(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp)),
-        factory = { playerView }
+    exoPlayer.addListener(
+        object : Player.Listener {
+            override fun onIsPlayingChanged(isPlayingValue: Boolean) {
+                isPlaying = isPlayingValue
+            }
+        }
     )
 
-    /** Cleanup the player when the composable is disposed of */
-    DisposableEffect(player) {
-        onDispose {
-            player.release()
+    DisposableEffect(
+        Box {
+            AndroidView(
+                factory = {
+                    PlayerView(context).apply {
+                        // Resizes the video in order to be able to fill the whole screen
+                        resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                        player = exoPlayer
+                        // Hides the default Player Controller
+                        useController = false
+                        // Fills the whole screen
+                        layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                    }
+                }
+            )
+
+            VideoLayout(
+                isPlaying = isPlaying,
+                onPlay = {
+                    exoPlayer.play()
+                },
+                onPause = {
+                    exoPlayer.pause()
+                }
+            )
+        }
+    ) {
+        onDispose { exoPlayer.release() }
+    }
+}
+@Composable
+fun VideoLayout(
+    isPlaying: Boolean,
+    onPlay: () -> Unit,
+    onPause: () -> Unit
+) {
+    AnimatedVisibility(
+        visible = !isPlaying,
+        enter = fadeIn(tween(200)),
+        exit = fadeOut(tween(200))
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 15.dp, horizontal = 20.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Favorite,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Call,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Send,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
         }
     }
 }
